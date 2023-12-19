44318a9bd217220e1eaf3b495ad492d2028061b6
MarioTawadrous <mario.tawadrous.95@gmail.com>
2023-12-19 22:06:03 +0200

    FIRST COMMIT

---
+++ portfolio/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css';
+function Header() {
+  return (
+    <div className='full-screen'>
+      <img className='img' src="src/assets/images/IMG20230305112531-1.jpg" />
+      <h1>- Mario Tawadrous -</h1>
+      <h2>Web Developer / Engineer</h2>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
